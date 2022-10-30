b885f0347191c1231b1fa20fca84e4f2c5b3e501
Thinh Phan <pseudothinhphan@gmail.com>
2022-10-29 19:14:06 -0700

    Add 'operate' function
    The 'operate' function will ask users for
    2 numbers and an operator (+, -, *, /).
    Then it will return the result based on
    users' choices.

--- js/calculator.js
+++ js/calculator.js
@@ -17,3 +17,21 @@ function divide(a, b) {
     let quotient = a / b;
     return quotient;
 }
+
+function operate(a, b, operator) {
+    a = prompt('a please: ');
+    let c = prompt('Choose an operator: ');
+    b = prompt('b please: ');
+    
+    if (c === '+') {
+        operator = add(a, b);
+    } else if (c === '-') {
+        operator = subtract(a, b);
+    } else if (c === '*') {
+        operator = multiply(a, b);
+    } else if (c === '/') {
+        operator = divide(a, b);
+    }
+
+    return operator;
+}
